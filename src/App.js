acb8f16eac561512518d5444d26ad0e5bbe2cf25
Trevor <trevdray@hotmail.com>
2023-05-11 11:24:21 -0700

    different max tries works

--- src/App.js
+++ src/App.js
@@ -10,58 +10,29 @@ import RuleList from './RuleList'
 function App() {
   const firstTry = 0
   const firstRule = "FirstRule"
+  const causeError = false
 
-  const [appStatus, setAppStatus] = useState('idle') // idle, running, completed
+  const [appStatus, setAppStatus] = useState('idle')
   const [responseStatus, setResponseStatus] = useState(null)
   const [currentRule, setCurrentRule] = useState(Object.values(rules).find(rule => rule.key === firstRule))
   const [tries, setTries] = useState(firstTry)
   const [ruleArray, setRuleArray] = useState([])
   const [progressPercentage, setProgressPercentage] = useState(Math.floor((tries / currentRule.maxTries) * 100))
 
-
   // having async / state problems here with changing statuses *** anticipating a problem here
-  const handleOnGo = async () => {
-    setAppStatus('running')
+  const handleGo = () => {
+    setAppStatus('running');
     if (appStatus === 'completed') {
-      handleOnReset()
-    }
-    let currentTries = firstTry;
-    while (currentTries < currentRule.maxTries) {
-      try {
-        // const response = await fetch("http://www.google.com");
-        // const status = await response.status();
-        let response = { status: 200 };
-        const status = response.status;
-        // this might result in a race condition during the real fetch requests
-        if (tries === firstTry) {
-          setResponseStatus(status);
-        } else {
-          setResponseStatus(prevStatus => (status !== prevStatus) ? status : prevStatus);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        currentTries++;
-        setTries(currentTries);
-
-      } catch (error) {
-        console.error(error);
-      }
+      handleReset();
     }
   }
-
-  // const handleOnClick = () => {
-  //   setAppStatus('running')
-  //   if (appStatus === 'completed') {
-  //     onReset()
-  //   }
-  //   onGo()
-  // }
 
   const handleRuleChange = () => {
     if (currentRule.passRule !== "END") {
       setCurrentRule(Object.values(rules).find(rule => rule.key === currentRule.passRule))
       setTries(firstTry)
       setResponseStatus(null)
-      handleOnGo()
+      handleGo()
     } else {
       setAppStatus("completed")
     }
@@ -70,24 +41,16 @@ function App() {
     }
   }
 
-  const handleOnReset = () => {
+  const handleReset = () => {
     setProgressPercentage(0)
     setRuleArray([])
     setCurrentRule(Object.values(rules).find(rule => rule.key === firstRule))
+    setTries(firstTry);
   }
 
   useEffect(() => {
-    console.log(
-      appStatus,
-      responseStatus,
-      currentRule,
-      tries,
-      ruleArray)
-  }, [appStatus,
-    responseStatus,
-    currentRule,
-    tries,
-    ruleArray]);
+    console.log(appStatus, responseStatus, currentRule, tries, ruleArray)
+  }, [appStatus, responseStatus, currentRule, tries, ruleArray]);
 
   useEffect(() => {
     setProgressPercentage(Math.floor((tries / currentRule.maxTries) * 100))
@@ -98,6 +61,32 @@ function App() {
       handleRuleChange();
     }
   }, [progressPercentage]);
+
+  // this is in a use effect so that we can get the most recent currentRule's maxTries 
+  useEffect(() => {
+    if (appStatus === 'running') {
+      (async () => {
+        let currentTries = tries;
+        while (currentTries < currentRule.maxTries) {
+          try {
+            console.log(currentRule.maxTries)
+            let response = { status: 200 };
+            const status = response.status;
+            if (tries === firstTry) {
+              setResponseStatus(status);
+            } else {
+              setResponseStatus(prevStatus => (status !== prevStatus) ? status : prevStatus);
+            }
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            currentTries++;
+            setTries(currentTries);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      })();
+    }
+  }, [currentRule, appStatus]);
 
   // console.log(currentRule) // single object
   // console.log(ruleArray) // object with a single object
@@ -111,9 +100,7 @@ function App() {
       </header>
       <ControlButton
         appStatus={appStatus}
-        setAppStatus={setAppStatus}
-        onGo={handleOnGo}
-        onReset={handleOnReset}
+        go={handleGo}
       />
       <FeedbackMessage
         appStatus={appStatus}
--- src/ControlButton.js
+++ src/ControlButton.js
@@ -1,12 +1,12 @@
 import Button from 'react-bootstrap/Button'
 
-const ControlButton = ({ appStatus, onGo }) => {
+const ControlButton = ({ appStatus, go }) => {
     return (
         <div style={appStatus !== "running" ? { padding: '20px' } : { padding: '5px' }}>
             {(appStatus === 'idle' || appStatus === 'completed') ? (
                 <Button
                     variant="primary"
-                    onClick={onGo}
+                    onClick={go}
                 >
                     Go
                 </Button>
