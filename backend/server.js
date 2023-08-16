f663e9086b243da266089b3b2abd2e36f7d9bf2c
Trevor <trevdray@hotmail.com>
2023-08-16 14:55:17 -0700

    include default csp

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,11 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
 
 // Create a connection to the database
 const pool = mariadb.createPool({
