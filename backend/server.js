af9f96947c42366862e348351c46eddf5085a48f
Trevor <trevdray@hotmail.com>
2023-08-23 11:18:29 -0700

    try catch the backend tested using burp suite, also sanitizing the cookie from the http request

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,12 @@ app.use((req, res, next) => {
 
 // Create a connection to the database
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 })
-
+  
 
 // Read the rules configuration file and store the values in separate arrays as safe values
 const rules = JSON.parse(fs.readFileSync(ruleConfigPath, 'utf8'))
@@ -47,62 +47,66 @@ const safeContinueOption = rulesArray.map(({ continueOption }) => continueOption
 const safeWarning = rulesArray.map(({ warning }) => warning)
 
 
-/**
- * Middleware to check if post data has been tampered with
- */
+
+// Middleware to check if post data has been tampered with
 const isPostDataTampered = (req, res, next) => {
-  const { uid, sequence, action, result } = req.body
-  const safeHttpResponses = /^[1-5]\d{2}$/
-  const safeUUIDLength = 36
-  const safeUUIDPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  try {
+    const { uid, sequence, action, result } = req.body
+    const safeHttpResponses = /^[1-5]\d{2}$/
+    const safeUUIDLength = 36
+    const safeUUIDPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    const cookies = req.headers.cookie
+    const userUuidCookie = cookies.split(';').find(cookie => cookie.trim().startsWith('user_uuid='))
+    const userUuid = userUuidCookie ? userUuidCookie.split('=')[1] : null
 
 
-  // Check if all sequence properties match the safe values defined earlier
-  const sequenceIsSafe = Object.values(sequence).every((item) => {
-    return (
-      safePorts.some((safePort) => safePort === item.port) &&
-      safeTitles.some((safeTitle) => safeTitle === item.title) &&
-      safeFailTexts.some((safeFailText) => safeFailText === item.failText) &&
-      safeFailRules.some((safeFailRule) => safeFailRule === item.failRule) &&
-      safePassRules.some((safePassRule) => safePassRule === item.passRule) &&
-      safeKeys.some((safeKey) => safeKey === item.key) &&
-      safeMaxTries.some((safeMaxTries) => safeMaxTries === item.maxTries) &&
-      safeContinueOption.some((safeContinueOption) => safeContinueOption === item.continueOption) &&
-      safeWarning.some((safeWarning) => safeWarning === item.warning) &&
-    ((safeHttpResponses).test(item.responseStatus) || item.responseStatus === null) // null due to current way of testing for failures
-    )
-  })
+    // Check if all sequence properties match the safe values defined earlier
+    const sequenceIsSafe = Object.values(sequence).every((item) => {
+      return (
+        safePorts.some((safePort) => safePort === item.port) &&
+        safeTitles.some((safeTitle) => safeTitle === item.title) &&
+        safeFailTexts.some((safeFailText) => safeFailText === item.failText) &&
+        safeFailRules.some((safeFailRule) => safeFailRule === item.failRule) &&
+        safePassRules.some((safePassRule) => safePassRule === item.passRule) &&
+        safeKeys.some((safeKey) => safeKey === item.key) &&
+        safeMaxTries.some((safeMaxTries) => safeMaxTries === item.maxTries) &&
+        safeContinueOption.some((safeContinueOption) => safeContinueOption === item.continueOption) &&
+        safeWarning.some((safeWarning) => safeWarning === item.warning) &&
+      ((safeHttpResponses).test(item.responseStatus) || item.responseStatus === null) // null due to current way of testing for failures
+      )
+    })
 
 
-  // Check if action and result properties are valid
-  const actionAndResultAreSafe = () => {
-    return (
-        (action === "restart" || action === "retry" || action === "start" || action === "continue") &&
-        (result === "incomplete" || result === "completed successfully" ||
-         result === "completed successfully with warning(s)" || result === "completed unsuccessfully")
-    )
+    // Check if action and result properties are valid
+    const actionAndResultAreSafe = () => {
+      return (
+          (action === "restart" || action === "retry" || action === "start" || action === "continue") &&
+          (result === "incomplete" || result === "completed successfully" ||
+          result === "completed successfully with warning(s)" || result === "completed unsuccessfully")
+      )
+    }
+
+
+    // Check if the UUID is valid
+    const uuidIsSafe = () => {
+      return ((safeUUIDPattern.test(uid) && uid.length === safeUUIDLength) && (safeUUIDPattern.test(userUuid) && userUuid.length === safeUUIDLength))
+    }
+
+
+    // Call the sanitization checks, and return a 405 status code (which cancels the request) if any of them return false
+    if (!sequenceIsSafe || !actionAndResultAreSafe() || !uuidIsSafe()) {
+      return res.status(405).json({ message: 'Data has been tampered with, ceasing request.' })
+    }
+      next()
+  } catch(err){
+    console.log(err, " Error in middleware")
+  }
 }
 
 
-  // Check if the UUID is valid
-  const uuidIsSafe = () => {
-    return safeUUIDPattern.test(uid) && uid.length === safeUUIDLength
-  }
-
-
-  // Call the sanitization checks, and return a 405 status code (which cancels the request) if any of them return false
-  if (!sequenceIsSafe || !actionAndResultAreSafe() || !uuidIsSafe()) {
-    return res.status(405).json({ message: 'Data has been tampered with, ceasing request.' })
-  }
-    next()
-}
-
-
-/**
- * Handle POST request to '/api/data'
- * Inserts the data into the database.
- */
+// Inserts the data into the database.
 app.post('/api/data', isPostDataTampered, async (req, res) => {
+  try {
     const { uid, sequence, action, result } = req.body
     const sequenceJson = JSON.stringify(sequence)
     let timestamp = new Date()
@@ -111,12 +115,15 @@ app.post('/api/data', isPostDataTampered, async (req, res) => {
         const query = `INSERT INTO ${tableName} (uuid, sequence, ip, action, result, timestamp, user_agent) VALUES (?, ?, ?, ?, ?, ?, ?)`
         const queryResult = await conn.query(query, [uid, sequenceJson, req.ip, action, result, timestamp, req.headers['user-agent']]);        res.status(200).json({ message: 'Data inserted successfully' })
     } catch (err) {
-        // throw err
         console.log(err)
         res.status(500).json({ message: 'Error inserting data' })
     } finally {
         if (conn) return conn.end()
     }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: " Error getting data from request body" })
+  }
 })
 
 
@@ -134,14 +141,25 @@ app.get('/api/rules', (req, res) => {
     }
 })
 
-// Serve the React app
+
 app.get('*', (req, res) => {
+  try {
     res.sendFile(path.join(buildPath, 'index.html'))
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(" Error serving React app")
+  }
 })
 
-app.listen(port, () => {
+
+try {
+  app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+  })
+} catch (err) {
+  console.log(err)
+  res.status(500).send("Error starting server")
+}
 
 
 // // Start the server
